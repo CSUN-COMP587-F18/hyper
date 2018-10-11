dad49b72f0723c81a4202e76f4b94577cd19903c
Alejandro Martinez <hello@alejandrom.xyz>
2018-10-11 01:15:42 -0700

    ESlint

--- test/unit/colors.test.js
+++ test/unit/colors.test.js
@@ -2,5 +2,5 @@ import test from 'ava';
 import colors from '../../app/utils/colors';
 
 test(`returns an updated object with modified color`, t => {
-  t.is(colors.getColorMap({white:'modified'}).white,'modified');
+  t.is(colors.getColorMap({white: 'modified'}).white, 'modified');
 });
